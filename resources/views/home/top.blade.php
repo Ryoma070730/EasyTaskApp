90e831abf36ef169fc36f149811a2bd49e6c915e
Ryoma <97567049+Ryoma070730@users.noreply.github.com>
2022-02-13 17:42:55 +0900

    First commit
    最初のコミットです

---
+++ resources/views/home/top.blade.php
@@ -0,0 +1,36 @@
+<head>
+    <link href="https://fonts.googleapis.com/css?family=M+PLUS+Rounded+1c" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+
+
+
+<header>
+<h1>タスク管理</h1>
+    <a href="/hello/new/" class="btn-new btn">
+        タスク追加
+    </a>
+</header>
+
+<main>
+    <div class="task-items">
+        @foreach ($posts as $post)
+            <div class="task">
+                <div class="task-text">
+                    <a href="/hello/{{ $post->id }}" class="btn-text">
+                        {{ $post->title }}
+                    </a>
+                </div>
+                <div class="task-btn">
+                    <a href="/hello/edit/{{ $post->id }}" class="btn-edit btn">
+                        編集
+                    </a>
+                    <a href="/hello/delete/{{ $post->id }}" class="btn-delete btn">
+                        削除
+                    </a>
+                </div>
+            </div>
+        @endforeach
+    </div>
+</main>
+
